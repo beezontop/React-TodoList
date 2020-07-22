df1f3bc1106cda493733fbf4bfd8c7c30747a6d1
Doris <d1882002@gmail.com>
2020-07-22 17:44:15 +0800

    Updates

---
+++ precache-manifest.f75cf00eb2fabcb9e44eb9d20e31c969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ea112e6568442ec74e07e1a4f457b3",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "2a86806db3f81b306540",
+    "url": "/React-TodoList/static/css/main.0cc3fe73.chunk.css"
+  },
+  {
+    "revision": "1bfe4a40ceb69a1d35ec",
+    "url": "/React-TodoList/static/js/2.31602944.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-TodoList/static/js/2.31602944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a86806db3f81b306540",
+    "url": "/React-TodoList/static/js/main.f768132a.chunk.js"
+  },
+  {
+    "revision": "239a8ae3283c26906a28",
+    "url": "/React-TodoList/static/js/runtime-main.3398c772.js"
+  }
+]);
